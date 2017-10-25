2042c45badffd00394b97dd554845bb8419ec197
James Donald <jdonald@leapmotion.com>
2017-10-24 23:37:07 -0700

    Add source files and .gitignore

---
+++ src/exe/resource.h
@@ -0,0 +1,34 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by crabs.rc
+//
+#define IDI_ICON1                       101
+#define IDI_ICONDOWN                    101
+#define IDI_ICON2                       102
+#define IDI_ICONLEFT                    102
+#define IDB_BITMAP1                     103
+#define IDI_ICON3                       104
+#define IDI_ICONRIGHT                   104
+#define IDI_ICONUP                      105
+#define IDI_ICONUPLEFT                  106
+#define IDI_ICONUPRIGHT                 107
+#define IDI_ICONDOWNLEFT                108
+#define IDI_ICONDOWNRIGHT               109
+#define IDR_MENU                        112
+#define IDM_MENU                        112
+#define IDD_ABOUTBOX                    113
+#define ID_POPUP_CLOSE                  40001
+#define ID_POPUP_ABOUT                  40002
+#define ID_POPUP_MOVEHOOK               40003
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_3D_CONTROLS                     1
+#define _APS_NEXT_RESOURCE_VALUE        114
+#define _APS_NEXT_COMMAND_VALUE         40004
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
